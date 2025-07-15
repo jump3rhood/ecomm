b01b1e342cd4db51fced43f5354a54bbbf516540
jump3rhood <mjm13121@gmail.com>
2025-07-15 23:53:15 +0530

    add application.properties.example file and add AccessDeniedExceptionHandler

--- user-service/src/main/java/org/john/personal/userservice/advices/AuthenticationExceptionHandler.java
+++ user-service/src/main/java/org/john/personal/userservice/advices/AuthenticationExceptionHandler.java
@@ -3,6 +3,7 @@ package org.john.personal.userservice.advices;
 import org.john.personal.userservice.exceptions.CustomAuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +23,14 @@ public class AuthenticationExceptionHandler {
     public ResponseEntity<?> handleRuntime(RuntimeException e) {
         System.out.println(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong!");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException e) {
+        System.out.println(e.getMessage());
+        Map<String, Object> map = new HashMap<>();
+        map.put("error", "insufficient_privileges");
+        map.put("message", e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(map);
     }
 }
